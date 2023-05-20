87b497ef48d10f84aaa85c4fd4206698091c6e2a
PauloRTrindade <prtrindadesf@gmail.com>
2023-05-20 10:26:00 -0300

    Product

---
+++ src/main/java/com/trindadeproject/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.trindadeproject.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trindadeproject.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
